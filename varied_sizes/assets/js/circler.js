a3fdcef419e23972fe9bd140497021b6e503b8f2
thedoodler <ramprakashsmail@gmail.com>
2020-11-05 19:38:18 +0530

    black and white

--- varied_sizes/assets/js/circler.js
+++ varied_sizes/assets/js/circler.js
@@ -1,20 +1,23 @@
-function circleMover(x,y,cursize,inval){
+function circleMover(x,y,cursize,inval,col){
   this.xpos = x;
   this.ypos = y;
   this.size = cursize;
   this.grow_rate = inval;
+  this.col = col;
 }
 
 circleMover.prototype.show = function(){
+  noStroke();
+  fill(this.col);
   circle(this.xpos,this.ypos,this.size);
 }
 
 circleMover.prototype.increaser = function(){
-  if (this.size > max_size || this.size < min_size){
-    if (this.size <= min_size-grow_rate){
+  if (this.size > max_limit || this.size < min_limit){
+    if (this.size <= min_limit-grow_rate){
       this.grow_rate = 1;
     }
-    else if (this.size >= min_size+grow_rate){
+    else if (this.size >= max_limit+grow_rate){
       this.grow_rate = -1;
     }
     else{
--- varied_sizes/assets/js/sketch.js
+++ varied_sizes/assets/js/sketch.js
@@ -2,12 +2,16 @@ var min_size = 10;
 var max_size  = 100;
 var size = 10;
 var grow_rate = 1;
+
+var max_limit = 150;
+var min_limit = 20;
 // var generation_diff = 2;
 
-var arr_length = 30;
+var arr_length = 10;
 
 var generation_diff = (max_size-min_size)/arr_length;
-var dot_arr;
+var white_arr;
+var black_arr;
 
 
 function create_2d_array(x_len,y_len){
@@ -18,11 +22,11 @@ function create_2d_array(x_len,y_len){
   return arr;
 }
 
-function fill_circle_arr(arr){
+function fill_circle_arr(arr,col,in_size,inv){
   for (let i=0; i<arr.length; i++){
     for (let j=0; j<arr[0].length; j++){
       // genval = i*generation_diff;
-      genval = map(i,0,arr.length,min_size,max_size);
+      genval = (inv) ? map(arr.length - i,0,arr.length,min_size,max_size) : map(i,0,arr.length,min_size,max_size);
       // cursize = (genval  > max_size) ? (max_size - (genval )%max_size) : genval ;
 
       cursize = (genval %(max_size*2) > max_size ) ? (max_size*2 - genval %(max_size*2)) : (genval %(max_size*2));
@@ -30,7 +34,7 @@ function fill_circle_arr(arr){
 
       // inval = (genval  > max_size) ? (-1) : (1);
       inval = (genval  > max_size && Math.floor(genval /max_size)%2 == 0) ? (-1*grow_rate) : (1*grow_rate);
-      arr[i][j] = new circleMover(i*max_size,j*max_size,cursize,inval);
+      arr[i][j] = new circleMover(in_size + (i)*max_size,in_size + (j)*max_size,cursize,inval,col);
     }
   }
   return arr;
@@ -43,8 +47,11 @@ function setup(){
   canvas.position(0,0);
   canvas.style('z-index', '-1');
 
-  dot_arr = create_2d_array(arr_length,arr_length);
-  dot_arr = fill_circle_arr(dot_arr);
+  white_arr = create_2d_array(arr_length,arr_length);
+  white_arr = fill_circle_arr(white_arr,'white',max_size*0,0);
+
+  black_arr = create_2d_array(arr_length,arr_length);
+  black_arr = fill_circle_arr(black_arr,'black',max_size*0.5,1);
 
 }
 
@@ -55,13 +62,20 @@ function draw(){
   noStroke();
   // fill(10);
 
-  for (let i=0; i<dot_arr.length; i++){
-    for (let j=0; j<dot_arr[0].length; j++){
-      dot_arr[i][j].increaser();
-      dot_arr[i][j].show();
+  for (let i=0; i<white_arr.length; i++){
+    for (let j=0; j<white_arr[0].length; j++){
+      white_arr[i][j].increaser();
+      white_arr[i][j].show();
     }
   }
 
+
+  for (let i=0; i<white_arr.length; i++){
+    for (let j=0; j<white_arr[0].length; j++){
+      black_arr[i][j].increaser();
+      black_arr[i][j].show();
+    }
+  }
 
 
 
