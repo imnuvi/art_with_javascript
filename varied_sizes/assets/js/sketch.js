dda05e83ba216b87b4a82d435032f3114e6b946f
thedoodler <ramprakashsmail@gmail.com>
2020-11-06 18:08:56 +0530

    spherical propagation

--- varied_sizes/assets/js/sketch.js
+++ varied_sizes/assets/js/sketch.js
@@ -8,7 +8,7 @@ var max_limit = 100;
 var min_limit = 10;
 // var generation_diff = 2;
 
-var arr_length = 20;
+var arr_length = 11;
 var grow_rate = (max_limit-min_limit)/arr_length/2;
 
 // var generation_diff = (max_size-min_size)/arr_length;
@@ -25,11 +25,17 @@ function create_2d_array(x_len,y_len){
 }
 
 function fill_circle_arr(arr,col,in_size,inv){
+  var loc_mid = createVector(Math.floor(arr.length/2),Math.floor(arr[0].length/2));
+  corner_dist = dist(loc_mid.x,loc_mid.y,0,0);
+
   for (let i=0; i<arr.length; i++){
     for (let j=0; j<arr[0].length; j++){
       // genval = i*generation_diff;
-      console.log(i);
-      genval = (inv) ? map(arr.length - i -1,0,arr.length,min_limit,max_limit,true) : map(i,0,arr.length,min_limit,max_limit,true);
+
+      // genval = (inv) ? map(arr.length*2 - i*j -1,0,arr.length,min_limit,max_limit,true) : map(j*i,0,arr.length,min_limit,max_limit,true);
+      cur_dis = dist(loc_mid.x,loc_mid.y,i,j);
+      genval = map(cur_dis,0.0,corner_dist,min_limit,max_limit,true);
+
       // cursize = (genval  > max_size) ? (max_size - (genval )%max_size) : genval ;
 
       // cursize = (genval %(max_size*2) > max_size ) ? (max_size*2 - genval %(max_size*2)) : (genval %(max_size*2));
@@ -55,7 +61,7 @@ function setup(){
   canvas.style('z-index', '-1');
 
   white_arr = create_2d_array(arr_length,arr_length);
-  white_arr = fill_circle_arr(white_arr,'white',max_size*0,0);
+  white_arr = fill_circle_arr(white_arr,'white',max_size*1,0);
 
   black_arr = create_2d_array(arr_length,arr_length);
   black_arr = fill_circle_arr(black_arr,'black',max_size*0.5,1);
@@ -74,7 +80,7 @@ function draw(){
 
   for (let i=0; i<white_arr.length; i++){
     for (let j=0; j<white_arr[0].length; j++){
-      white_arr[i][j].increaser();
+      // white_arr[i][j].increaser();
       white_arr[i][j].show();
     }
   }
