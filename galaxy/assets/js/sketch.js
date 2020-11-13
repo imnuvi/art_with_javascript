e943df394fb35e87c72a48a20a3631c7f64a7d73
thedoodler <ramprakashsmail@gmail.com>
2020-11-13 21:59:14 +0530

    added star data structure

--- galaxy/assets/js/sketch.js
+++ galaxy/assets/js/sketch.js
@@ -11,13 +11,13 @@ function setup(){
   canvas = createCanvas(ww,wh);
   canvas.style('z-index','-1');
   canvas.position(0,0);
+	background(0);
+
+  for (let i=0; i<40; i++){
+	  circle(random_position()[0],random_position()[1],random(0,20));
+  }
 }
 
 function draw(){
-	background(0);
-	if (frameCount % 20 == 0 ){
-	  circle(random_position()[0],random_position()[1],random(0,20));
-
-	}
 
 }
