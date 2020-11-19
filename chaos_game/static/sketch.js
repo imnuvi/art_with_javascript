51ae225e5637d30c6e34142d16027e6bb6f4f849
thedoodler <ramprakashsmail@gmail.com>
2020-11-19 22:27:50 +0530

    random lerp function finished

--- chaos_game/static/sketch.js
+++ chaos_game/static/sketch.js
@@ -14,6 +14,7 @@ function reportsize(){
 function point_generator(n){
   // n is the number of points required
   point_arr = [];
+  noFill();
   beginShape();
   for(let i=0; i<n; i++){
     xx = random(ww);
@@ -41,17 +42,20 @@ function setup(){
 
   stroke(255);
   strokeWeight(5);
+  background(0);
   point_array = point_generator(n);
   curx = random(ww);                        // current random value for x, keeps changing
   cury = random(wh);
 }
 
 function draw(){
-  val = random(Math.round(random(n)));
-  for (let i=0; i<n; i++){
 
-  }
-  point()
+  val = Math.floor(random(n));
+  console.log(val);
+  curx = lerp(curx,point_array[val*2],0.5);
+  cury = lerp(curx,point_array[val*2 + 1],0.5);
+
+  point(curx,cury);
   // triangle(ax,ay,bx,by,cx,cy);
   // circle(mouseX,mouseY,100);
 
