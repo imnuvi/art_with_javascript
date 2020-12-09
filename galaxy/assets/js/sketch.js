9427954166f59a95945f7a0d64eb513d7f6782b3
thedoodler <ramprakashsmail@gmail.com>
2020-12-09 22:11:26 +0530

    ADD: increased star count and negative speed

--- galaxy/assets/js/sketch.js
+++ galaxy/assets/js/sketch.js
@@ -1,6 +1,12 @@
 let star_list = [];
-let count = 200;
+let count = 400;
 var speed
+
+function random_color(){
+  thecol = color(random(0,255),random(190,255),random(200,255));
+  return thecol;
+}
+
 
 function random_position(){
 	return [random(0,ww),random(0,wh)];
@@ -23,7 +29,7 @@ function draw(){
 		translate(ww/2,wh/2);
 		background(0);
 		angleMode(DEGREES);
-		speed = map(mouseX,0,ww,0.2,50);
+		speed = map(mouseX,0,ww,-50,50);
 		rotation = map(mouseY,0,wh,0,50*PI);
 		rotate(rotation);
 	  for (let i=0; i<count; i++){
--- galaxy/assets/js/star.js
+++ galaxy/assets/js/star.js
@@ -3,20 +3,27 @@ function Star(){
   this.y = random(-wh,wh);
   this.z = random(ww);
   this.size = map(this.z,0,ww,0,20);
+  this.col = random_color();
 }
 
 Star.prototype.update = function(){
   this.z -= speed;
   if (this.z<=1){
-    this.z = width;
+    this.z = ww;
     this.x = random(-ww/2,ww/2);
     this.y = random(-wh,wh);
+  }
+  else if (this.z>ww){
+      this.z = 0;
+      this.x = random(-ww/2,ww/2);
+      this.y = random(-wh,wh);
   }
   // this.size = map(this.z,0,ww,0,20);
 }
 
 Star.prototype.show = function(){
   noStroke();
+  fill(this.col);
   sx = map(this.x / this.z, 0, 1, 0, ww);
   sy = map(this.y / this.z, 0, 1, 0, wh);
   this.size = map(this.z,0,ww,20,0);
