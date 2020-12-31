b306703a34bf6f037cb44bdde63208059b31d469
cosmoglint <ramprakashsmail@gmail.com>
2020-12-31 18:37:41 +0530

    finish  spawner and remover

--- confetti/static/confetti.js
+++ confetti/static/confetti.js
@@ -1,11 +1,14 @@
 function Confetti(x,y){
   this.alive = true;
   this.pos = createVector(x,y);
+
+  this.color = random_color();
+  this.grav = random(2,gravity_value);
+
   this.max_width = random(20,40);
   this.max_height = random(20,40);
   this.max_rot = random(20,60);
   this.max_x_dev = random(80,120);
-  this.color = random_color();
 
 
   this.width_vel = random(0.5,4);
@@ -17,8 +20,6 @@ function Confetti(x,y){
   this.width = random(-this.max_width,this.max_width);
   this.height = random(-this.max_height,this.max_height);
   this.rot = random(0,this.max_rot);
-  // this.max_x_dev = random(30,100);
-  // this.size = 20;
 }
 
 Confetti.prototype = {
@@ -32,7 +33,7 @@ Confetti.prototype = {
   },
 
   update: function(){
-    this.pos.y += gravity_value;
+    this.pos.y += this.grav;
     this.width_vel = this.maxmin(this.width,this.width_vel,-this.max_width,this.max_width) ? this.width_vel * (-1) : this.width_vel;
     this.height_vel = this.maxmin(this.height,this.height_vel,-this.max_height,this.max_height) ? this.height_vel * (-1) : this.height_vel;
     this.rot_vel = this.maxmin(this.rot,this.rot_vel,-this.max_rot,this.max_rot) ? this.rot_vel * (-1) : this.rot_vel;
@@ -44,8 +45,10 @@ Confetti.prototype = {
     this.pos.x += this.x_vel;
     this.x_moved += this.x_vel;
 
-    // this.pos.x += this.x_moved;
-    // console.log(this.x_vel);
+
+    if (this.pos.y > wh){
+      this.alive = false;
+    }
   },
 
   show: function(){
--- confetti/static/sketch.js
+++ confetti/static/sketch.js
@@ -1,5 +1,7 @@
 let gravity_value = 5;
 
+let confetti_array = [];
+
 function random_color(){
   thecol = color(random(0,255),random(0,255),random(0,255));
   return thecol;
@@ -12,18 +14,37 @@ function init(){
   canvas = createCanvas(ww,wh);
   canvas.style('z-index','-1');
   canvas.position(0,0);
+
+  for(let i=0; i<30; i++){
+    confetti_array.push(new Confetti(random(ww),random(-500,0)));
+  }
 }
 
 function setup(){
   pixelDensity(2);
   angleMode(DEGREES);
   init();
-  conf = new Confetti(ww/2,0);
+  // conf = new Confetti(ww/2,0);
 }
 
 function draw(){
   background(0);
   // circle(mouseX,mouseY,100);
-  conf.update();
-  conf.show();
+  for (conf of confetti_array){
+    if (conf.alive){
+      conf.update();
+      conf.show();
+    }
+    else{
+      ind = confetti_array.indexOf(conf);
+      confetti_array.splice(ind,1);
+    }
+  }
+
+  if (confetti_array.length < 100){
+    for (let i=0; i<10; i++){
+      confetti_array.push(new Confetti(random(ww),0))
+    }
+  }
+  // console.log(confetti_array);
 }
