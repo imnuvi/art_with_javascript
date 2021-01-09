626419de74cf4b1cc3551cc3535fbe9b4dd5c262
cosmoglint <ramprakashsmail@gmail.com>
2021-01-09 11:20:21 +0530

    ADD:  lorenz equations

--- lorenz_attractor/static/sketch.js
+++ lorenz_attractor/static/sketch.js
@@ -2,17 +2,17 @@ let x = 0;
 let y = 0;
 let z = 0;
 
-let a = 0;
-let b = 0;
-let c = 0;
-
-
+// Prandtl number
+let a = 1;
+// Rayleigh number
+let b = 1;
+// parameters
+let c = 1;
 
 
 
 function random_color(){
-  thecol = color(random(0,255),random(0,255),random(0,255));
-  return thecol;
+  thecol = color(random(0,255),random(0,255),random(0,255));  return thecol;
 }
 
 function init(){
@@ -21,6 +21,7 @@ function init(){
   canvas = createCanvas(ww,wh);
   canvas.style('z-index','-1');
   canvas.position(0,0);
+  background(0);
 }
 
 function setup(){
@@ -29,5 +30,12 @@ function setup(){
 }
 
 function draw(){
-  circle(mouseX,mouseY,100);
+
+  dt = 0.001;
+  dx = (a * ( y - x )) * dt;
+  dy = (x * ( b - z ) - y) * dt;
+  dz = (x * y - c * z ) * dt;
+
+  stroke(255);
+  // circle(mouseX,mouseY,100);
 }
