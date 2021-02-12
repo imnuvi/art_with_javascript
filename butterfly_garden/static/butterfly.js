7d976ebcd780673a01e83dd344f2de4dce0efd60
ram prakash <ramprakashsmail@gmail.com>
2021-02-12 17:51:46 +0530

    ADD:  bubbling bubbles

--- butterfly_garden/static/butterfly.js
+++ butterfly_garden/static/butterfly.js
@@ -2,6 +2,12 @@ function Butterfly(xpos,ypos){
   this.pos = createVector(xpos,ypos);
 }
 
-Butterfly.prototype.show = function(){
-  circle(this.pos.x,this.pos.y,100);
+Butterfly.prototype = {
+  show: function(){
+    circle(this.pos.x,this.pos.y,100);
+  },
+
+  update: function(){
+    this.pos = this.pos.add(p5.Vector.random2D());
+  }
 }
