8b96782626c736c37815e5e400d41aa84e7a9317
cosmoglint <ramprakashsmail@gmail.com>
2021-10-18 22:50:18 +0530

    ADD:  chaotic balls

--- chaotic_balls/static/diff_equation.js
+++ chaotic_balls/static/diff_equation.js
@@ -1,29 +1,17 @@
 g = 9.8;
 tmax = 10;
-
-reflected = ReflectionTransform[{-x[t], -y[t]}][{ x'[t], y'[t]}];
-
-
-solveSystem[x0, y0] := NDSolveValue[
-  {
-    y''[t] == g,
-    x''[t] == 0,
-    x'[0] == 0,
-    y'[0] == 0,
-    x[0] == x0,
-    y[0] == y0,
-    WhenEvent[x[t]2 + y[t]2 == 1,
-  {x'[t] , y'[t] } -> Evaluate[reflected]]
-},
-(x,y),
-(t,0,tmax)
-];
+radius = 100;
+center = createVector(ww/2, wh/2);
 
 
-function displace(x, y, displacement){
+export function displace(x, y, displacement){
   new_x = x + displacement.x
   new_y = y + displacement.y
-  // if new_x 
+  new = createVector(new_x, new_y)
+  if (new_x - center.x)**2 + (new_y - center.y)**2 >= radius{
+    print(yeah)
+  }
+  return new
 }
 
 function SolveSystem(x0, y0){
@@ -32,7 +20,3 @@ function SolveSystem(x0, y0){
   x_d = 0;
   y_d = 0;
 }
-
-
-xf1, xf2 = solveSystem[0.01, 0.5];
-xf2, yf2 = solveSystem[0.0012, 0.5];
--- chaotic_balls/static/elastic_ball.js
+++ chaotic_balls/static/elastic_ball.js
@@ -3,6 +3,7 @@ function bouncyBall(radius, color, bounciness){
     this.color = color;
     this.bounciness = bounciness;
     this.initial_pos = new p5.Vector(ww/2, wh/2);
+    this.center = new p5.Vector(ww/2, wh/2);
     this.cur_pos = this.initial_pos.copy();
 }
 
@@ -12,11 +13,26 @@ function calculate_displacement(){
 }
 
 bouncyBall.prototype = {
+    displace: function(x, y, displacement){
+        let moved_x = x + displacement.x;
+        let moved_y = y + displacement.y;
+        // let moved = createVector(moved_x, moved_y);
+        let moved = createVector(displacement.x, displacement.y);
+        console.log(moved,this.radius)
+        console.log((moved_x - this.center.x)**2 + (moved_y - this.center.y)**2)
+        inflection = createVector(-5,-5)
+        if ((moved_x - this.center.x)**2 + (moved_y - this.center.y)**2 >= home_radius) {
+          moved.mult(inflection);
+        }
+        return moved
+      },
     update: function(){
         let disp = calculate_displacement();
         let dispx = disp[0];
         let dispy = disp[1];
         displacement = createVector(dispx,dispy);
+        move = createVector(1,1);
+        displacement = this.displace(this.cur_pos.x, this.cur_pos.y, move)
         this.cur_pos.add(displacement)
     },
     render: function(){
