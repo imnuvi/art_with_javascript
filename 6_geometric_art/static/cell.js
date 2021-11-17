b90df415f5703f5ccb2b85ad47874474b19bca60
cosmoglint <ramprakashsmail@gmail.com>
2021-11-18 00:58:14 +0530

    ADD: arrow function for cubes

--- 6_geometric_art/static/cell.js
+++ 6_geometric_art/static/cell.js
@@ -15,7 +15,7 @@ cell.prototype = {
     rect(this.xpos, this.ypos, this.width, this.height)
     fill(this.color)
     noStroke();
-    opts = [ "render_triangle", "render_duality" ]
+    opts = [ "render_triangle", "render_duality" , "render_squarrow"]
     orientation = Math.floor(Math.random() * 4)
     this[opts[Math.floor(Math.random() * opts.length)]](orientation)
   },
@@ -59,18 +59,34 @@ cell.prototype = {
   },
   render_squarrow: function(orientation){
     if (orientation == 0){
-      vals = []
+      locs = [this.helper_function(1),this.helper_function(5)]
+      vals = [locs[0][0], locs[0][1], locs[1][0], locs[1][1]]
     }
+    else if (orientation == 1){
+      locs = [this.helper_function(2),this.helper_function(6)]
+      vals = [locs[0][0], locs[0][1], locs[1][0], locs[1][1]]
+    }
+    else if (orientation == 2){
+      locs = [this.helper_function(4),this.helper_function(8)]
+      vals = [locs[0][0], locs[0][1], locs[1][0], locs[1][1]]
+    }
+    else if (orientation == 3){
+      locs = [this.helper_function(5),this.helper_function(9)]
+      vals = [locs[0][0], locs[0][1], locs[1][0], locs[1][1]]
+    }
+    rect(vals[0], vals[1], vals[2], vals[3])
   },
   helper_function: function(location_number){
-    location_map = [(0,0),(0,1),(0,2),(1,0),(1,1),(1,2),(2,1),(2,2),(2,3)]
+    let location_map = [[0,0],[0,1],[0,2],[1,0],[1,1],[1,2],[2,1],[2,2],[2,3]]
     // given a keypad number (1-9) this function returns the respective x and y co ordinates for that location. eg: 1 - (0,0),  2 - (width/2, 0) , 8 - (width/2, height) and so on
-    unit = this.size/2
-    yloc = location_map[location_number][0]
-    xloc = location_map[location_number][1]
-    xval = unit * xloc 
-    yval = unit * yloc 
-    return (x_val, y_val)
+    location_number = location_number - 1 // deviation for list index:w
+
+    let unit = this.size/2
+     let yloc = location_map[location_number][0]
+     let xloc = location_map[location_number][1]
+     let x_val = this.xpos +  unit * xloc 
+     let y_val = this.ypos + unit * yloc 
+    return [x_val, y_val]
   }
 
 }
