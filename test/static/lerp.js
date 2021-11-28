ea336d7de735d487eebcabf3463832a51797eb53
cosmoglint <ramprakashsmail@gmail.com>
2021-11-29 01:04:16 +0530

    START:  linear interpolation stuff

---
+++ test/static/lerp.js
@@ -0,0 +1,6 @@
+// this function performs linear interpolation between two shapes.
+
+// idea is to create a generic function that does this for all shapes.
+function shape(x1, y1, x2, y2){
+    
+}
