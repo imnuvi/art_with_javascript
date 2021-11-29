f6e667053c368639e53bb3f81eda12f13753882c
cosmoglint <ramprakashsmail@gmail.com>
2021-11-30 02:10:56 +0530

    ADD:  helper functions for lerp test

--- test/static/lerp.js
+++ test/static/lerp.js
@@ -4,3 +4,16 @@
 function shape(x1, y1, x2, y2){
     
 }
+
+let n = 100;
+
+// this function will generate a list of points for any shape given
+function points_generator(shape_obj){
+  if (shape_obj){
+    shape = shape_obj.shape;
+    if (shape == 'circle'){
+      center = shape_obj.center;
+      rad = shape_obj.radius;
+    }
+  }
+}
