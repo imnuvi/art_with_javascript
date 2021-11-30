be7bfbced4b9f26709f3dbb7aaaee65da3171fea
cosmoglint <ramprakashsmail@gmail.com>
2021-12-01 01:21:36 +0530

    ADD:  points getter for lerping

--- test/static/lerp.js
+++ test/static/lerp.js
@@ -12,8 +12,21 @@ function points_generator(shape_obj){
   if (shape_obj){
     shape = shape_obj.shape;
     if (shape == 'circle'){
+      // for a circle the center and radius are enough to recreate it
       center = shape_obj.center;
       rad = shape_obj.radius;
+    }
+    else if (shape == 'square'){
+      // for a square either a corner and length are required or top left and bottom right corners are required
+      top_left_corner = shape_obj.top_left_corner;
+      bottom_right_corner = shape_obj.bottom_right_corner;
+      length = shape_obj.length;
+    }
+    else if (shape == 'triangle'){
+      // for a triangle we need the three corners
+      corner_1 = shape_obj.corner_1;
+      corner_2 = shape_obj.corner_2;
+      corner_3 = shape_obj.corner_3;
     }
   }
 }
